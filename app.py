cde85de2de15963bddbb538f007157dd0f3f560c
Fresbin Thomas <fresbinthomas@gmail.com>
2019-11-03 14:45:05 +0200

    starting project - 191AEM009

--- app.py
+++ app.py
@@ -8,6 +8,5 @@ class Hello(Resource):
     def get(self):
         return 'Hello World'
 
-api.add_resource(Hello, '/hello') #http://127.0.0.1:5000/student/Ingrida
-
+api.add_resource(Hello, '/hello') #http://127.0.0.1:5000/hello
 app.run(port=5000)
