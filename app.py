4788390785eae9e2c9a7e8ce86388bebc4c035bf
Fresbin Thomas <fresbinthomas@gmail.com>
2019-11-03 14:35:31 +0200

    starting project - 191AEM009

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Hello(Resource):
+    def get(self):
+        return 'Hello World'
+
+api.add_resource(Hello, '/hello') #http://127.0.0.1:5000/student/Ingrida
+
+app.run(port=5000)
