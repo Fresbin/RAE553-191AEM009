b3fe3607fe967f6677580e8f772a5069d9dbf9ca
Fresbin Thomas <fresbinthomas@gmail.com>
2019-11-23 14:20:11 +0200

    Collection app.py.

--- app.py
+++ app.py
@@ -21,6 +21,15 @@ class Types(Resource):
     def delete(self, type_name):
         del catalog_list[type_name]
         return catalog_list
+    def post(self, type_name):
+        if type_name in catalog_list:
+            parser = reqparse.RequestParser()
+            parser.add_argument("price")
+            args = parser.parse_args()
+            catalog_list[type_name]['price'] = args['price']
+            return catalog_list[type_name]
+        else:
+            return "Error {} type is not in catalog list".format(type_name)   
 api.add_resource(Catalog, '/catalog_list') #http://127.0.0.1:5000/catalog_list
 api.add_resource(Types, '/type/<type_name>') #http://127.0.0.1:5000/type_name
 app.run(host='0.0.0.0',port=5000)
