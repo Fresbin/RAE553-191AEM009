e00d2c0ee80b6735ebba3008eac13feea9e29c31
Fresbin Thomas <fresbinthomas@gmail.com>
2019-12-11 14:50:44 +0200

    updated with test price tables.py

--- code/tables.py
+++ code/tables.py
@@ -1,16 +1,15 @@
 import sqlite3
 
 connection = sqlite3.connect('data.db')
-
 cursor = connection.cursor()
 
-
-
-create_table = "CEATE TABLE IF NOT EXIST users (id INTEGER PRIMARY KEY, username text, password text)"
+reate_table = "CEATE TABLE IF NOT EXIST users (id INTEGER PRIMARY KEY, username text, password text)"
 cursor.execute(create_table)
 
 create_table = "CREATE TABLE IF NOT EXISTS items (name text PRIMARY KEY, price real)"
 cursor.execute(create_table)
+
+cursor.execute("INSERT INTO items VALUES ('test, 10.99')")
 
 connection.commit()
 
