dfae458d1a4a6d812de766762eb43776722a6194
Fresbin Thomas <fresbinthomas@gmail.com>
2019-12-12 20:42:05 +0200

    code updated on 12-12-19 app.py

--- code/app.py
+++ code/app.py
@@ -3,40 +3,17 @@ from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
+from user import UserRegister
+from catalog import catalog , catalog_listList
+
 app = Flask(__name__)
 app.secret_key = 'toms@77'
 api = Api(app)
-jwt = JWT(app, authenticate, identity)
-catalog_list={"type1":{"price":10},"type2":{"price":20},"type3":{"price":30}}
 
-class Catalog(Resource):
-    @jwt_required()
-    def get(self):
-        return catalog_list
-class Types(Resource):
-    @jwt_required()
-    def get(self, type_name ):
-        return catalog_list[type_name]
-    def put(self, type_name):
-        if type_name not in catalog_list:
-            catalog_list[type_name] = {"price": ""}
-        parser = reqparse.RequestParser()
-        parser.add_argument("price")
-        args = parser.parse_args()
-        catalog_list[type_name]['price'] = args['price']
-        return catalog_list[type_name]
-    def delete(self, type_name):
-        del catalog_list[type_name]
-        return catalog_list
-    def post(self, type_name):
-        if type_name in catalog_list:
-            parser = reqparse.RequestParser()
-            parser.add_argument("price")
-            args = parser.parse_args()
-            catalog_list[type_name]['price'] = args['price']
-            return catalog_list[type_name]
-        else:
-            return "Error {} type is not in catalog list".format(type_name)
-api.add_resource(Catalog, '/catalog_list') #http://127.0.0.1:5000/catalog_list
+jwt = JWT(app, authenticate, identity)
+
+api.add_resource(Catalog, '/catalog_list') #http://127.0.0.1:5000/catalog_list/<type_name>
 api.add_resource(Types, '/type/<type_name>') #http://127.0.0.1:5000/type_name
+api.add_resource(UserRegister, '/register') #http://127.0.0.1:5000/register
+
 app.run(host='0.0.0.0',port=5000)
