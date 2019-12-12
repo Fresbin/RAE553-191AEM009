e633155c1e2519e8a2bf1b8c1d16d0440fb3e00d
Fresbin Thomas <fresbinthomas@gmail.com>
2019-12-12 21:59:17 +0200

    code updated on 12-12-19 app.py

--- code/app.py
+++ code/app.py
@@ -4,7 +4,8 @@ from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
 from user import UserRegister
-from catalog import catalog , catalog_listList
+from catalog import Catalog , catalog_list, Types
+
 
 app = Flask(__name__)
 app.secret_key = 'toms@77'
@@ -12,7 +13,7 @@ api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
 
-api.add_resource(Catalog, '/catalog_list') #http://127.0.0.1:5000/catalog_list/<type_name>
+api.add_resource(Catalog, '/catalog_list') #http://127.0.0.1:5000/catalog_list
 api.add_resource(Types, '/type/<type_name>') #http://127.0.0.1:5000/type_name
 api.add_resource(UserRegister, '/register') #http://127.0.0.1:5000/register
 
